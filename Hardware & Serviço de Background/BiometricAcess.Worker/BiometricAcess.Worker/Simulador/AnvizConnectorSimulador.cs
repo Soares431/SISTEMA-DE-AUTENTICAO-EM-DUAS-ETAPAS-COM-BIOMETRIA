798c21218bf3d80f1a892d722465bd47a8610087
Gustavo Henrique <gh33493@gmail.com>
2026-05-01 22:55:49 -0300

    HW-02: AnvizConnector com simulador e Worker Service funcionando
    Conecção com T50M criada, juntamente com com meios para simular o funcionamento do T50M com eventos aleatorios para fim de teste. As mudanças entre o modo simulado e o real devem ser feitas no Program.cs. Como padrão esta no modo simulado.

---
+++ Hardware & Serviço de Background/BiometricAcess.Worker/BiometricAcess.Worker/Simulador/AnvizConnectorSimulador.cs
@@ -0,0 +1,35 @@
+﻿using BiometricAcess.Worker.Models;
+using BiometricAcess.Worker.Services;
+
+namespace BiometricAcess.Worker.Simulador
+{
+    internal class AnvizConnectorSimulador : IAnvizConnector
+    {
+        private bool _conectado = false;
+
+        public bool Conectar()
+        {
+            _conectado = true;
+            Console.WriteLine("Simulador: Conectado ao T50M (simulado)");
+            return true;
+        }
+
+        public EventoAcesso? BuscarNovoEvento()
+        {
+            if (!_conectado)
+            {
+                return null;
+            }
+            else
+            {
+                return T50MSimulador.gerarEvento();
+            }
+        }
+
+        public void Desconectar()
+        {
+            _conectado = false;
+            Console.WriteLine("Simulador: Desconectado do T50M (simulado)");
+        }
+    }
+}
---
+++ Hardware & Serviço de Background/BiometricAcess.Worker/BiometricAcess.Worker/Simulador/T50MSimulador.cs
@@ -0,0 +1,42 @@
+﻿using BiometricAcess.Worker.Models;
+namespace BiometricAcess.Worker.Simulador{
+    internal class T50MSimulador{
+        private static readonly Random _random = new Random();
+
+        public static EventoAcesso gerarEvento()
+        {
+
+
+            int pessoaID = _random.Next(1, 6);
+            string tipoVerificacao;
+            if (_random.Next(2) == 0)
+            {
+                tipoVerificacao = "digital_id";
+            }
+            else
+            {
+                tipoVerificacao = "senha_id";
+            }
+            bool acessoLiberado;
+
+            if (_random.Next(2) == 0)
+            {
+                acessoLiberado = true;
+            }
+            else
+            {
+                acessoLiberado = false;
+            }
+
+            return new EventoAcesso
+            {
+                PessoaID = pessoaID,
+                TipoVerificacao = tipoVerificacao,
+                AcessoLiberado = acessoLiberado,
+                DataHora = DateTime.Now
+            };
+
+
+        }
+    }
+}
