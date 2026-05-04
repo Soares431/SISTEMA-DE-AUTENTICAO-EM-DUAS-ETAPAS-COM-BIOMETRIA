f81d2c2f6f779b511628426a3dace2aeeffc549f
Gustavo Henrique <gh33493@gmail.com>
2026-05-03 23:12:31 -0300

    Remoção de comentario
    Comentario informativo foi removido do codigo e adicionado ao readmi

--- Hardware & Serviço de Background/BiometricAcess.Worker/BiometricAcess.Worker/Program.cs
+++ Hardware & Serviço de Background/BiometricAcess.Worker/BiometricAcess.Worker/Program.cs
@@ -4,8 +4,6 @@ using BiometricAcess.Worker.Simulador;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// Registra o simulador como implementação da interface
-// Quando tiver o hardware, troca AnvizConnectorSimulador por AnvizConnector
 builder.Services.AddSingleton<IAnvizConnector, AnvizConnectorSimulador>();
 
 builder.Services.AddHostedService<Worker>();
