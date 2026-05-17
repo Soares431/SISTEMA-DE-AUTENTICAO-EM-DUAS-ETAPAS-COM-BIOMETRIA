7764c94bbe554b8f3083a481eb4297fb55fd4cd7
DevManga893 <leagueoflegends0319@gmail.com>
2026-05-17 16:46:47 -0300

    Correções Do Documento Enviado(Usando O Banco De Lucas)

--- InfraestruturaBloco1/Program.cs
+++ InfraestruturaBloco1/Program.cs
@@ -13,34 +13,12 @@ builder.Services.AddScoped<EmailService>();
 builder.Services.AddScoped<PasswordService>();
 builder.Services.AddScoped<AesService>();
 builder.Services.AddScoped<AuditService>();
-builder.Services.AddScoped<TokenService>();
 
 // Configuração do EF Core com SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Configuração JWT
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
 
-builder.Services.AddAuthorization();
 
 // Adicionar suporte a controllers e Swagger/OpenAPI
 builder.Services.AddControllers();
