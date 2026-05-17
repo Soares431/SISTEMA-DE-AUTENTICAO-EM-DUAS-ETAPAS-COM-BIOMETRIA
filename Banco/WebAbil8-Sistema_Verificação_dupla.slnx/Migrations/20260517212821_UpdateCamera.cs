2d92ffaee76fabb79d13a7619672fc7bcedc648a
Lucas Soares <soares00431@gmail.com>
2026-05-17 18:29:01 -0300

    add camera repository e implemetion

---
+++ Banco/WebAbil8-Sistema_Verificação_dupla.slnx/Migrations/20260517212821_UpdateCamera.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAbil8_Sistema_Verificação_dupla.slnx.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCamera : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ativa",
+                table: "camera",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ativa",
+                table: "camera");
+        }
+    }
+}
