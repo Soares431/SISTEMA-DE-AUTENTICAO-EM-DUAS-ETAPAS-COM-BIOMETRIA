d5920042d8cdc8789e8b80d3303f96b0f2879866
Gustavo Henrique <gh33493@gmail.com>
2026-05-16 21:47:07 -0300

    Arduino - script de compilação e configuração Wokwi VS Code funcionando

---
+++ Hardware & Serviço de Background/BiometricAcess.Worker/BiometricAcess.Worker/Arduino/compilar.ps1
@@ -0,0 +1,3 @@
+﻿$arduinoCli = "C:\Users\gp254\AppData\Local\Programs\Arduino IDE\resources\app\lib\backend\resources\arduino-cli.exe"
+& $arduinoCli compile --fqbn arduino:avr:uno --output-dir build Arduino.ino
+Write-Host "Compilado!" -ForegroundColor Green
