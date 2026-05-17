c549198775af3fb0d7a0015bde5e064ae63b6a5b
DevManga893 <leagueoflegends0319@gmail.com>
2026-05-17 17:19:04 -0300

    Correção Program.cs

--- InfraestruturaBloco1/Program.cs
+++ InfraestruturaBloco1/Program.cs
@@ -1,50 +1,39 @@
-
 using InfraestruturaBloco1.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using InfraestruturaBloco1.Data;
+using WebAbil8_Sistema_Verificação_dupla.slnx.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Registrar serviços no container de injeção de dependência
+// Registrar serviços
 builder.Services.AddScoped<EmailService>();
 builder.Services.AddScoped<PasswordService>();
 builder.Services.AddScoped<AesService>();
 builder.Services.AddScoped<AuditService>();
+builder.Services.AddScoped<CameraService>();
 
-// Configuração do EF Core com SQL Server
+// Configuração do EF Core com SQLite (banco do Int1)
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-
-// Adicionar suporte a controllers e Swagger/OpenAPI
+// Controllers e Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configuração do pipeline HTTP
+// Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Infraestrutura Bloco 1 API v1");
-        c.RoutePrefix = string.Empty; // abre o Swagger direto na raiz (http://localhost:5111)
+        c.RoutePrefix = string.Empty;
     });
 }
 
 app.UseHttpsRedirection();
-
-// Ativar autenticação e autorização
-app.UseAuthentication();
 app.UseAuthorization();
-
-// Mapear controllers automaticamente
 app.MapControllers();
-
 app.Run();
